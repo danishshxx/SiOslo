a24e995b0bd53212aa1d093a07060ff3ba2e9487
Foidaishu <Hudzaifah8050@gmail.com>
2026-08-20 23:05:29 +0700

    fixing integeration with the proposed front end api

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
-from app.api.v1.api import api_router # <--- Import agregator v1 yang baru
+from fastapi.middleware.cors import CORSMiddleware
+from app.api.v1.api import api_router
 
 app = FastAPI(title="SiOslo API")
 
-# Daftarin semua rute v1 dengan prefix /api/v1
-app.include_router(api_router, prefix="/api/v1") 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+ )
+
+app.include_router(api_router, prefix="/api/v1")
 
 @app.get("/health")
 def health_check():
